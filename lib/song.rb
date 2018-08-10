94e8d21d2c198b0c8b581241da67e34739d71879
Karen C <chenyanhuan66@163.com>
2018-08-10 07:01:10 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+  attr_accessor :artist
+ 
+  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+end
