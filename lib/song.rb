17ee8267224ea14d7d6ede17a4c129c355589ab4
Karen C <chenyanhuan66@163.com>
2018-08-10 07:11:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,6 @@ class Song
     @name = name
   end
 
-
   def artist_name=(name)
     if (self.artist.nil?)
       self.artist = Artist.new(name)
@@ -13,4 +12,13 @@ class Song
       self.artist.name = name
     end
   end
+
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+  
 end
